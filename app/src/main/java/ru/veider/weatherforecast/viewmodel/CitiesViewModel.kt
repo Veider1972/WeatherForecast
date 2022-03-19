4632e273571f4d1cd12834ee371a9ff13822fd16
Корховой Вадим <veider@inbox.ru>
2022-03-19 19:27:27 +0300

    Состояние на 19.03.2022. Добавлено 30 минутное кэширование данных прогноза погоды на основе Room

--- app/src/main/java/ru/veider/weatherforecast/viewmodel/CitiesViewModel.kt
+++ app/src/main/java/ru/veider/weatherforecast/viewmodel/CitiesViewModel.kt
@@ -1,8 +1,6 @@
 package ru.veider.weatherforecast.viewmodel
 
-import android.content.SharedPreferences
 import android.preference.PreferenceManager
-import androidx.core.content.edit
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.veider.weatherforecast.data.DataLoading
@@ -35,10 +33,10 @@ class CitiesViewModel(
         }.start()
     }
 
-    constructor() : this(MutableLiveData(), CitiesRepositoryImpl()) {
-        when(PreferenceManager.getDefaultSharedPreferences(WeatherApplication.getInstance()).getBoolean(CITIES_KEY,true)){
-            true -> dataLoading = RUSSIAN
-            false -> dataLoading = FOREIGN
+    init {
+        dataLoading = when(PreferenceManager.getDefaultSharedPreferences(WeatherApplication.getInstance()).getBoolean(CITIES_KEY,true)){
+            true -> RUSSIAN
+            false -> FOREIGN
         }
     }
 }
